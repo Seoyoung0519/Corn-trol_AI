5e78bcdc597ae155dbd80e6bd1f757b67babe3e2
seoyoung <a01032402839@gmail.com>
2026-05-05 17:47:11 +0900

    upload nockup files

---
+++ data/mock_records_example.py
@@ -0,0 +1,11 @@
+# 실제 데이터는 로컬에서만 관리하세요
+
+MOCK_RECORDS = [
+    {
+        "recordId": 1,
+        "userId": 1,
+        "content": "example content",
+        "topic": "example",
+        "keywords": ["example"]
+    }
+]
